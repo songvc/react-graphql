ce97c6c3ef71bf833551b0e25284e79a7c3fcdf8
v <v@vs-MacBook-Pro.local>
2020-03-26 16:14:25 -0700

    add login page

--- client/src/index.js
+++ client/src/index.js
@@ -1,4 +1,22 @@
 import React from 'react';
 import { render } from 'react-dom';
+import { createGlobalSTyle } from 'styled-components';
+import App from './App';
 
-render(<div></div>, document.getElementById('app'));
+const GlobalStyle = createGlobalStyle`
+    html, body, #app {
+        height: 100%;
+        margin: 0;
+        padding: 0;
+        width: 100%;
+    }
+
+    body {
+        font-family: monospace;
+    }
+`
+
+
+render(<><GlobalStyle>
+        <App/>
+    </GlobalStyle></>, document.getElementById('app'));
